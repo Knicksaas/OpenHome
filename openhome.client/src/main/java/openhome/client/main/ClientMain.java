ebf1cf86ac83c7b453cf725d43552b8af2eebe11
Nickitiki <46298023+Nickitiki@users.noreply.github.com>
2021-06-13 22:40:30 +0200

    Basic project structure

---
+++ openhome.client/src/main/java/openhome/client/main/ClientMain.java
@@ -0,0 +1,8 @@
+package openhome.client.main;
+
+public class ClientMain {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ openhome.server/src/main/java/openhome/server/main/ServerMain.java
@@ -0,0 +1,8 @@
+package openhome.server.main;
+
+public class ServerMain {
+
+    public static void main(String[] args) {
+
+    }
+}
